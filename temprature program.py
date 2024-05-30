a6e0ef10f5cbe294050fb2ba2a5c542b8c14d96d
Zoha Emaan <163720799+ZohaEmaan01@users.noreply.github.com>
2024-05-31 00:53:39 +0500

    temprature conversion

---
+++ temprature program.py
@@ -0,0 +1,8 @@
+def tempratureinfahrenheit():
+
+    C = float(input("enter temprature in celsius: "))
+    fahrenheit = 9 / 5 * C + 32
+    print("temprature in farenheit is: ", fahrenheit)
+
+
+tempratureinfahrenheit()
