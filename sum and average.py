20ea08d7902a5346067363311cadd48f4faa8074
Zoha Emaan <163720799+ZohaEmaan01@users.noreply.github.com>
2024-06-07 16:36:47 +0500

    sum and average

---
+++ sum and average.py
@@ -0,0 +1,13 @@
+sum = 0
+count = 0
+n = 0
+while n < 5 :
+    number = int(input('enter a number: '))
+    sum = sum + number
+    count = count + 1
+    average = sum / count
+    n = n + 1
+print(sum)
+print(average)
+
+
